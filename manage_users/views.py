276e4cf96e5c4635b66196301c0f4b6c032610c1
tskillian <tskillian@gmail.com>
2013-09-16 20:26:42 -0700

    experimenting

--- manage_users/views.py
+++ manage_users/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello world!")
-	#return render(request, 'home.html')
+	return render(request, 'home.html')
