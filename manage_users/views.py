f9ee2615fc9943b86a819b4a0ce2ec8de17c4dbb
tskillian <tskillian@gmail.com>
2013-09-16 21:39:04 -0700

    experimenting

--- manage_users/views.py
+++ manage_users/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from manage_users.models import Users
+
 
 def index(request):
-	return render(request, 'home.html')
+	#return render(request, 'home.html')
+
+	users_list = Users.objects.order_by('-user_last_name')
+	template = loader.get_template('home.html')
+	context = RequestContext(request, {
+		'users_list': users_list,
+		})
+	return HttpResponse(template.render(context))
+
