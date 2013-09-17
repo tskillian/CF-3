6e4b412adce1336bb684354a7fc9fb4ff6ecef9b
tskillian <tskillian@gmail.com>
2013-09-16 19:04:40 -0700

    experimenting

--- manage_users/urls.py
+++ manage_users/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', include('manage_users.urls'),
+    url(r'^polls/', include('manage_users.urls'),
     # url(r'^hellodjango/', include('hellodjango.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
