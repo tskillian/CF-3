5f088b42f5cf233e327dd1ea4572d2642362f23f
tskillian <tskillian@gmail.com>
2013-09-17 19:54:09 -0700

    experimenting

--- manage_users/models.py
+++ manage_users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Users(models.Model):
 	user_first_name = models.CharField(max_length=255)
 	user_last_name = models.CharField(max_length=255)
 	email_address = models.CharField(max_length=255)
+
+class UserForm(ModelForm):
+	class Meta:
+		model = Users
+		fields = ['user_first_name', 'user_last_name', 'email_address']
