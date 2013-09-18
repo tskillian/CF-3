b41fcac9a54dbdfa72df20d88a175c7b13690be7
tskillian <tskillian@gmail.com>
2013-09-17 23:11:52 -0700

    experimenting

--- manage_users/views.py
+++ manage_users/views.py
@@ -15,15 +15,15 @@ def index(request):
 	#test.save()
 	users_list = Users.objects.all()
 	if request.method == 'POST':
-		form = UserForm(request.POST)
-		if form.is_valid():
+		#form = UserForm(request.POST)
+		#if form.is_valid():
 			#user_first_name = request.POST.get('user_first_name', '')
 			#user_last_name = request.POST.get('user_last_name', '')
 			#email_address = request.POST.get('email_address', '')
 			#users_obj = Users(user_first_name=user_first_name, user_last_name=user_last_name, email_address=email_address)
 			#users_obj.save()
-			form.save()
-			return HttpResponseRedirect('/')
+		UserForm().save()
+		return HttpResponseRedirect('/')
 	else:
 		form = UserForm()
 	return render(request, 'home.html', {
