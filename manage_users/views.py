442c3197dfa53ff8b6c916c754252c3d74b0174b
tskillian <tskillian@gmail.com>
2013-09-18 17:46:40 -0700

    experimenting

--- manage_users/views.py
+++ manage_users/views.py
@@ -27,9 +27,9 @@ def index(request):
 			#users_obj.save()
 		#UserForm().save()
 			user_first_name = form.cleaned_data.get('user_first_name', '')
-        	user_last_name = form.cleaned_data.get('user_last_name', '')
-            email_address = form.cleaned_data.get('email_address', '')
-            users_obj = Users(user_first_name=user_first_name,
+			user_last_name = form.cleaned_data.get('user_last_name', '')
+			email_address = form.cleaned_data.get('email_address', '')
+			users_obj = Users(user_first_name=user_first_name,
                     user_last_name=user_last_name, 
                     mail_address=email_address)
             users_obj.save()
