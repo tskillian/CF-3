cd3dbd4059a6c4b67e17702600237eecceaf06ef
tskillian <tskillian@gmail.com>
2013-09-21 12:46:11 -0700

    experimenting

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -111,7 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/hellodjango/manage_users/templates"
+    "/home/hellodjango/manage_users/templates",
+    "/home/hellodjango/users/templates"
 )
 
 INSTALLED_APPS = (
