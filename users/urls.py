b08abd00cc2c05d0b15b5a56719406c94cb05bb6
tskillian <tskillian@gmail.com>
2013-09-21 14:10:27 -0700

    experimenting

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from users import views
 urlpatterns = patterns('',
 	url(r'^$', views.UserList.as_view(), name='user_list'),
 	url(r'^new$', views.UserCreate.as_view(), name='user_new'),
-	url(r'^edit/(?P<pk>\d+)$', views.UserDelete.as_view(), name='user_edit'),
+	url(r'^edit/(?P<pk>\d+)$', views.UserUpdate.as_view(), name='user_edit'),
 	url(r'^delete/(?P<pk>\d+)$', views.UserDelete.as_view(), name='user_delete')
 	)
