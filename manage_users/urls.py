0456c8dfa06d396f92cb33997d65bede32c245ff
tskillian <tskillian@gmail.com>
2013-09-22 15:59:52 -0700

    update

--- manage_users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from manage_users import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^index/', views.index, name='index')
-)
